53470989ed12a1728c6e01e679aa3b0af688358b
子轩 林 <2394974737@qq.com>
2023-01-02 19:50:18 +0800

    Site updated: 2023-01-02 19:50:17

---
+++ js/weather.js
@@ -0,0 +1,19 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>"CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "000000",
+    "tmpSize": "16",
+    "cityColor": "000000",
+    "citySize": "16",
+    "aqiColor": "000000",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "dbb12c9e5dee4077b76a0d94518f5be1"
+  }
